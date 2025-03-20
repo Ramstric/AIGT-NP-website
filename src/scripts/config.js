b6150fca4d710705935f533feae923247289e999
Ramon Hdez <ramstric@gmail.com>
2025-03-20 00:24:48 -0600

    New commit for current version of the project, which utilizes word2vec trained models for text detection.

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,1 +1,3 @@
 export const backendURL = "https://api-demo-2-182443968648.northamerica-northeast1.run.app";
+/*
+export const backendURL = "http://127.0.0.1:8080";*/
--- src/scripts/predict-model.js
+++ src/scripts/predict-model.js
@@ -2,7 +2,6 @@ import { backendURL } from "./config";
 export { predictModel };
 
 const lrModelResult = document.getElementById('lr');
-const nbModelResult = document.getElementById('nb');
 const rfModelResult = document.getElementById('rf');
 const svmModelResult = document.getElementById('svm');
 
@@ -13,15 +12,25 @@ async function predictModel(text) {
     })   .then((response) => {
         response.json().then((data) => {
             console.log(data);
-            lrModelResult.innerText = data.logistic_regression_model === 1 ? 'AI' : 'Human';
-            nbModelResult.innerText = data.naive_bayes_model === 1 ? 'AI' : 'Human';
-            rfModelResult.innerText = data.random_forest_model === 1 ? 'AI' : 'Human';
-            svmModelResult.innerText = data.support_vector_model === 1 ? 'AI' : 'Human';
+            
+            /*
+            rfModelResult.firstElementChild.innerText = data.LogisticRegression === 1 ? 'AI' : 'Human';
 
-            lrModelResult.style.color = data.logistic_regression_model === 1 ? 'var(--red)' : 'var(--blue)';
-            nbModelResult.style.color = data.naive_bayes_model === 1 ? 'var(--red)' : 'var(--blue)';
-            rfModelResult.style.color = data.random_forest_model === 1 ? 'var(--red)' : 'var(--blue)';
-            svmModelResult.style.color = data.support_vector_model === 1 ? 'var(--red)' : 'var(--blue)';
+            rfModelResult.firstElementChild.innerText = (data.RandomForest_proba[0]*100).toFixed(2) + '% Human'
+            rfModelResult.lastElementChild.innerText = (data.RandomForest_proba[1]*100).toFixed(2) + '% AI'
+
+            svmModelResult.firstElementChild.innerText = data.SVC === 1 ? 'AI' : 'Human';
+            svmModelResult.firstElementChild.style.color = data.SVC === 1 ? 'var(--red)' : 'var(--blue)';
+            */
+            lrModelResult.getElementsByTagName("tr")[0].getElementsByTagName("td")[1].innerText = (data.LogisticRegression_proba[0]*100).toFixed(2) + '%'
+            lrModelResult.getElementsByTagName("tr")[1].getElementsByTagName("td")[1].innerText = (data.LogisticRegression_proba[1]*100).toFixed(2) + '%'
+
+            rfModelResult.getElementsByTagName("tr")[0].getElementsByTagName("td")[1].innerText = (data.LogisticRegression_proba[0]*100).toFixed(2) + '%'
+            rfModelResult.getElementsByTagName("tr")[1].getElementsByTagName("td")[1].innerText = (data.LogisticRegression_proba[1]*100).toFixed(2) + '%'
+
+            svmModelResult.getElementsByTagName("tr")[0].getElementsByTagName("td")[0].innerText = data.SVC === 1 ? 'AI' : 'Human';
+            svmModelResult.getElementsByTagName("tr")[0].getElementsByTagName("td")[0].style.color = data.SVC === 1 ? 'var(--red)' : 'var(--blue)';
+
         });
         
     }).catch((error) => {
